0b721cff314c2403c299187a56c3aa39c4a29e91
Pew_Booom <ghistyo@gmail.com>
2022-03-03 00:22:50 -0300

    minor name changes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ensebast <ensebast@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 22:08:00 by ensebast          #+#    #+#             */
-/*   Updated: 2022/03/02 23:25:17 by ensebast         ###   ########.br       */
+/*   Updated: 2022/03/03 00:22:29 by ensebast         ###   ########.br       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,15 @@ int	main(void)
 	return (0);
 }
 
+// SIGQUIT = ctrl + d must be ignored
+// SIGINT = ctrl + c will call the function signal_handler
 static void	init_signal(void)
 {
-	signal(SIGINT, signal_handler);
+	signal(SIGINT, interrupt_handler);
 	signal(SIGQUIT, SIG_IGN);
 }
 
-static void	signal_handler(int sig)
+static void	interrupt_handler(int sig)
 {
 	write(1, "\n", 1);
 	rl_replace_line("", 1);
