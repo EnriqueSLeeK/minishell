176f98fd15b5a6d3938412c0883641c7db671276
Pew_Booom <ghistyo@gmail.com>
2022-03-02 23:48:20 -0300

    prompt color change and 42 header inserted

--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: ensebast <ensebast@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 22:05:58 by ensebast          #+#    #+#             */
-/*   Updated: 2022/03/02 23:43:26 by ensebast         ###   ########.br       */
+/*   Updated: 2022/03/02 23:47:37 by ensebast         ###   ########.br       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*prompt(void)
 {
 	char	*line;
 
-	line = readline("\e[32m minishell \e[01;32m>\e[00m ");
+	line = readline("\e[1;34m minishell \e[01;34m>\e[00m ");
 	if (line == 0)
 	{
 		rl_clear_history();
