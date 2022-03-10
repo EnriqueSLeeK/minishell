c5b3a76ee9566ec01cee7a20e3b88c98deb10334
Moacir Amaro <moacir@gmail.com>
2022-03-10 10:45:22 -0300

    fix norm

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -1,10 +1,22 @@
-#ifndef TOKENIZER_H
-#define TOKENIZER_H
-#include "shell.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 10:44:59 by mamaro-d          #+#    #+#             */
+/*   Updated: 2022/03/10 10:45:12 by mamaro-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define BULTIN 42
-#define INFILE 43
-#define OUTFILE 44
-#define PIPE 45
+#ifndef TOKENIZER_H
+# define TOKENIZER_H
+# include "shell.h"
+
+# define BULTIN 42
+# define INFILE 43
+# define OUTFILE 44
+# define PIPE 45
 
 #endif
