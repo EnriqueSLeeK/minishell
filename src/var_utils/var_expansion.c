da0d922b99a4da373b59c0aa639dc8cae83a22b5
j <ghistyo@gmail.com>
2022-03-16 21:15:13 -0300

    var_expansion UPDATE

--- src/var_utils/var_expansion.c
+++ src/var_utils/var_expansion.c
@@ -6,7 +6,7 @@
 /*   By: ensebast <ensebast@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 13:44:03 by ensebast          #+#    #+#             */
-/*   Updated: 2022/03/16 20:39:26 by ensebast         ###   ########.fr       */
+/*   Updated: 2022/03/16 21:13:38 by ensebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,32 +44,50 @@ static char *seach_var(char *var_name)
 	return (buff);
 }
 
-// Expansion when mixed with string
-static void	expand_mix(char **parsed_line, int k)
+static char *prepare(char *parsed_line, char *var_name, int k)
 {
 	char	*tmp;
 	char	*line;
 	char	*buff;
+
+	tmp = ft_calloc(k + 1, sizeof(char));
+	ft_strlcpy(tmp, parsed_line, k + 1);
+	if (var_name[0] == '?')
+	{
+		buff = status_code();
+		line = ft_strjoin(tmp, buff);
+		free(buff);
+		free(tmp);
+		buff = ft_strjoin(line, &(var_name[1]));
+		free(line);
+	}
+	else
+	{
+		buff = ft_strdup(seach_var(var_name));
+		line = ft_strjoin(tmp, buff);
+		free(buff);
+		free(tmp);
+		buff = ft_strjoin(line, &(var_name[find_dllsign(var_name)]));
+		free(line);
+	}
+	return (buff);
+}
+
+// Expansion when mixed with string
+static void	expand_mix(char **parsed_line, int k, int flag)
+{
+	char	*line;
 	char	*var_name;
 
 	if (k == -1 || (*parsed_line)[k] != '$')
 		return ;
 	var_name = &((*parsed_line)[k + 1]);
 	(*parsed_line)[k] = 0;
-	tmp = ft_calloc(k + 1, sizeof(char));
-	ft_strlcpy(tmp, *parsed_line, k + 1);
-	if (var_name[0] == '?' && var_name[1] == 0)
-		buff = status_code();
-	else
-		buff = ft_strdup(seach_var(var_name));
-	line = ft_strjoin(tmp, buff);
-	free(buff);
-	free(tmp);
-	buff = ft_strjoin(line, &(var_name[find_dllsign(var_name)]));
+	line = prepare(*parsed_line, var_name, k);
 	free(*parsed_line);
-	*parsed_line = buff;
-	free(line);
-	expand_mix(parsed_line, find_dllsign(*parsed_line));
+	*parsed_line = line;
+	if (flag != EXPAND_ONE)
+		expand_mix(parsed_line, find_dllsign(*parsed_line), flag);
 }
 
 void	var_expansion(char **parsed_line, int flag)
@@ -83,7 +101,7 @@ void	var_expansion(char **parsed_line, int flag)
 		k = find_dllsign(parsed_line[i]);
 		if (k != -1 && parsed_line[i][k] == '$')
 		{
-			expand_mix(&parsed_line[i], k);
+			expand_mix(&parsed_line[i], k, flag);
 			if (flag == EXPAND_ONE)
 				break ;
 		}
