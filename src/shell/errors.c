a6c3d42e3ce35c68d5579d0ab82b2b531536b433
Moa <moacirjgt@gmail.com>
2022-05-02 19:09:35 -0300

    fix some leaks on / and .

--- src/shell/errors.c
+++ src/shell/errors.c
@@ -6,20 +6,23 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 10:34:11 by mamaro-d          #+#    #+#             */
-/*   Updated: 2022/04/21 19:53:12 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2022/05/02 19:07:51 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-/* int		check_next_relation(t_node *node)
-{
-} */
 void	show_error(char *name, char *message, int status, int exit_from_shell)
 {
+	char	*tmp;
+
+	tmp = ft_strrchr(message, '/');
 	write(2, "Minishell: ", 12);
 	write(2, name, ft_strlen(name));
-	write(2, message, ft_strlen(message));
+	if(tmp)
+		write(2, tmp + 1, ft_strlen(tmp));
+	else
+		write(2, message, ft_strlen(message));
 	write(2, "\n", 1);
 	g_data.exit_code = status;
 	if (exit_from_shell)
